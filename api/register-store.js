07c9811a05dc8297c59308f427f5a30252225908
jbuc247 <jbuc247@gmail.com>
2026-06-26 03:11:05 +0300

    Fix api/register-store.js to use ES modules

--- api/register-store.js
+++ api/register-store.js
@@ -1,6 +1,6 @@
-const { createClient } = require('@libsql/client');
+import { createClient } from '@libsql/client/web';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Add CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
